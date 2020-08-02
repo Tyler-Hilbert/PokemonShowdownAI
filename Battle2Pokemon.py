456abdf7a1ec34dc67582a87b98e7ad565f261ed
Tyler-Hilbert <tylerhilbert@Tylers-MacBook-Pro.local>
2020-08-01 23:31:43 -0600

    Improved method for computing weaknesses

--- Battle2Pokemon.py
+++ Battle2Pokemon.py
@@ -32,8 +32,9 @@ class Pokedex:
 
 # Saves information about a Pokemon
 class Pokemon:
-    def __init__(self, name, types):
+    def __init__(self, name, types, weakness):
         self.name = name
+        self.weakness = weakness
         typesLst = types.split('"')
         self.types = []
         while len(typesLst) > 1:
@@ -47,18 +48,28 @@ class Pokemon:
         return s
 
     def outcomeDefending(self, attackingPokemon):
-        s = self.name + " is defending against " + attackingPokemon.name
+        s = self.name + " is defending against " + attackingPokemon.name + "\n"
+        for w in self.weakness:
+            for ap in attackingPokemon.types:
+                if w == ap:
+                    s += self.name + " weak against " + ap
+        # FIXME - is there another comparision
+        print (s)
+
+        '''
         if attackingPokemon.name.lower() == "pidgey" and self.name.lower() == "pikachu":
             return "2 - win - pidgey is not very effective"
         elif attackingPokemon.name.lower() == "pikachu" and self.name.lower() == "pidgey":
             return "1 - lose - pikachu is super effective"
-
+        '''
+    '''
     def outcomeAttacking(self, defendingPokemon):
         s = self.name + " is attacking against " + defendingPokemon.name
         if self.name.lower() == "pidgey" and defendingPokemon.name.lower() == "pikachu":
             return "2 - win - pidgey is not very effective"
         elif self.name.lower() == "pikachu" and defendingPokemon.name.lower() == "pidgey":
             return "1 - lose - pikachu is super effective"
+    '''
 ##### class Pokedex #####
 
 
@@ -67,7 +78,9 @@ set = 0
 
 if set == 0:
     allyPkmnName = "Pidgey"
+    allyWeakAgainst = ["Electric", "Ice", "Rock"]
     foePkmnName = "Pikachu"
+    foeWeakAgainst = []
 elif set == 1:
     allyPkmnName = "Umbreon"
     foePkmnName = "Blaziken"
@@ -83,16 +96,20 @@ elif set == 4:
 
 pokedex = Pokedex()
 
-allyPkmn = Pokemon(pokedex.findPokemonUsingName(allyPkmnName)['name'], pokedex.findPokemonUsingName(allyPkmnName)['types']) # FIXME - Pokemon Loading
-foePkmn = Pokemon(pokedex.findPokemonUsingName(foePkmnName)['name'], pokedex.findPokemonUsingName(foePkmnName)['types']) # FIXME - Pokemon Loading
+allyPkmn = Pokemon(pokedex.findPokemonUsingName(allyPkmnName)['name'], pokedex.findPokemonUsingName(allyPkmnName)['types'], allyWeakAgainst) # FIXME - Pokemon Loading
+foePkmn = Pokemon(pokedex.findPokemonUsingName(foePkmnName)['name'], pokedex.findPokemonUsingName(foePkmnName)['types'], foeWeakAgainst) # FIXME - Pokemon Loading
 
-print ("you are: ")
+print ("Ally: ")
 print (allyPkmn.toString())
 print ("")
-print ("against: ")
+print ("Foe: ")
 print(foePkmn.toString())
 print ("")
 
+print("\n(allyPkmn.outcomeDefending(foePkmn))")
+print(allyPkmn.outcomeDefending(foePkmn))
+
+'''
 print("\n(allyPkmn.outcomeAttacking(foePkmn))")
 print(allyPkmn.outcomeAttacking(foePkmn))
 print("\n(allyPkmn.outcomeDefending(foePkmn))")
@@ -101,3 +118,4 @@ print("\n(foePkmn.outcomeAttacking(allyPkmn))")
 print(foePkmn.outcomeAttacking(allyPkmn))
 print("\n(foePkmn.outcomeDefending(allyPkmn))")
 print(foePkmn.outcomeDefending(allyPkmn))
+'''
