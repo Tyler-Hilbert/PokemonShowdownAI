981c1825912ebd9b558fedb4c2ebb83319864802
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-08-07 19:49:42 -0600

    Reordered classes

--- CompareTeamsTypesMatchups.py
+++ CompareTeamsTypesMatchups.py
@@ -15,8 +15,50 @@ gameStr = "Dugtrio / Scyther / Slowbro-Galar / Tsareena / Incineroar / Inteleon"
 
 
 ##########  CLASSES  ####################################################################
-#########################################################################################
+#########################################################################################ßß
+# Saves information about a Pokemon
+class Pokemon:
+    def __init__(self, name, types):
+        # Set name and type
+        self.name = name
+        typesLst = types.split('"')
+        self.types = []
+        while len(typesLst) > 1:
+            self.types.append(typesLst[1])
+            typesLst = typesLst[2:]
+
+        # Set weaknesses
+        self.weaknesses = [] # TODO - calculate this on the spot
+        for type in self.types:
+            # FIXME - Add way for 4 times weakness
+            typeStr = type.lower() + "Defending"
+            self.addWeaknesses(typeStr)
+
+     # FIXME - passing in key a better way
+    def addWeaknesses(self, defending):
+        self.weaknesses.append(defendingMatchups[defending])
+        # FIXME - adding weakness (ultimately this should be calculated on the spot rather than a variable)
+
+    def toString(self):
+        # TODO - add more data to print
+        s = self.name
+        for type in self.types:
+            s += "\t" + type
+        return s
+
+    # Returns True if the pokemon is weak against attackingPokemon
+    def isWeak(self, attackingPokemon):
+        for attackType in attackingPokemon.types:
+            for allyPokemonKey in self.weaknesses:
+                for key in allyPokemonKey:
+                    if allyPokemonKey[key] == SUPER_EFFECTIVE and key == attackType:
+                        return True
+        return False
+##### class Pokedex #####
+
+
 # Loads and stores Pokedex data
+# TODO - convert the data into a friendlier format to read in Python and remove this string parsing
 class Pokedex:
     # Loads self.Pokdex
     def __init__(self):
@@ -48,46 +90,7 @@ class Pokedex:
                 return p
 ##### class Pokedex #####
 
-# Saves information about a Pokemon
-class Pokemon:
-    def __init__(self, name, types):
-        # Set name and type
-        self.name = name
-        typesLst = types.split('"')
-        self.types = []
-        while len(typesLst) > 1:
-            self.types.append(typesLst[1])
-            typesLst = typesLst[2:]
 
-        # Set weaknesses
-        self.weaknesses = [] # TODO - calculate this on the spot
-        for type in self.types:
-            # FIXME - Add way for 4 times weakness
-            typeStr = type.lower() + "Defending"
-            self.addWeaknesses(typeStr)
-            
-     # FIXME - passing in key a better way
-    def addWeaknesses(self, defending):
-        self.weaknesses.append(defendingMatchups[defending])
-        # FIXME - adding weakness (ultimately this should be calculated on the spot rather than a variable)
-
-    def toString(self):
-        # TODO - add more data to print
-        s = self.name
-        for type in self.types:
-            s += "\t" + type
-        return s
-
-    # Returns True if the pokemon is weak against attackingPokemon
-    def isWeak(self, attackingPokemon):
-        for attackType in attackingPokemon.types:
-            for allyPokemonKey in self.weaknesses:
-                for key in allyPokemonKey:
-                    if allyPokemonKey[key] == SUPER_EFFECTIVE and key == attackType:
-                        return True
-        return False
-
-##### class Pokedex #####
 
 #########################################################################################
 #########################################################################################
