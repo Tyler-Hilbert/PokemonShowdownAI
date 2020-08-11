4dcee82a44a9dd5a0ff87dce56e987bc322aa9ad
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-08-10 23:23:28 -0600

    Select Pokemon based on difference between strengths and weaknesses

--- SelectPokemon.py
+++ SelectPokemon.py
@@ -7,10 +7,12 @@ from TypeMatchups_data import *
 ##########  INPUT VARIABLES  ############################################################
 #########################################################################################
 # Enter a string that contains the name of all your pokemon (can contain non-pokemon characters/words and isn't formatted)
-allyPkmnNameLst = "jolteon, umbreon, espeon, leafeon, vaporeon, flareon"
+allyPkmnNameLst = "jolteon bisharp espeon leafeon vaporeon flareon"
 # Enter a string that contains the name of all your opponent pokemon (can contain non-pokemon characters/words and isn't formatted)
-foePkmnNameLst = "test Dugtrio / Scyther / Slowbro-Galar / Tsareena / Incineroar / Inteleon"
+foePkmnNameLst = "Incineroar / Skarmory / Kyurem / Mamoswine / Tyranitar / Celebi"
 # FIXME - "Silvally-*" could be parsed wrong. Check for other odd cases on the name
+# FIXME - galar pokemon don't work either
+# FIXME - do alola work? Maybe I just have to filter out '-'
 #########################################################################################
 #########################################################################################
 
@@ -28,16 +30,30 @@ class Pokemon:
             self.types.append(typesLst[1])
             typesLst = typesLst[2:]
 
+        # FIXME - cases where pokemon has one weakness to a pokemon and a strength to the same pokemon
+        # TODO - set weaknesses and strengths in same loop
         # Set weaknesses
         self.weaknesses = [] # TODO - calculate this on the spot
         for type in self.types:
             # FIXME - Add way for 4 times weakness
             typeStr = type.lower() + "Defending"
-            self.addWeaknesses(typeStr)
+            self.addWeakness(typeStr)
+
+        # Set strengths
+        self.strengths = [] # TODO - calculate this on the spot
+        for type in self.types:
+            # FIXME - Add way for immunity
+            typeStr = type.lower() + "Defending"
+            self.addStrength(typeStr)
 
      # FIXME - passing in key a better way
-    def addWeaknesses(self, defending):
+    def addWeakness(self, defending):
         self.weaknesses.append(defendingMatchups[defending])
+        # FIXME - adding weakness (ultimately this should be calculated on the spot rather than a variable)
+
+     # FIXME - passing in key a better way
+    def addStrength(self, defending):
+        self.strengths.append(defendingMatchups[defending])
         # FIXME - adding weakness (ultimately this should be calculated on the spot rather than a variable)
 
     def toString(self):
@@ -55,6 +71,16 @@ class Pokemon:
                     if allyPokemonKey[key] == SUPER_EFFECTIVE and key == attackType:
                         return True
         return False
+
+    # Returns True if the attacking pokemon is not very effective
+    def isStrong(self, attackingPokemon):
+        for attackType in attackingPokemon.types:
+            for allyPokemonKey in self.strengths:
+                for key in allyPokemonKey:
+                    if ( (allyPokemonKey[key] == NOT_VERY_EFFECTIVE) or (allyPokemonKey[key] == IMMUNE) ) and key == attackType:
+                        return True
+        return False
+
 ##### class Pokedex #####
 
 
@@ -129,32 +155,39 @@ pokedex = Pokedex()
 foePkmnLst = getPokemonInStr(foePkmnNameLst, pokedex)
 allyPkmnLst = getPokemonInStr(allyPkmnNameLst, pokedex)
 
-leastWeaknesses = 99 # Arbitrary number always too high to be least weaknesses
-lstWeakPkmnName = "PLACEHOLDER"
+highestNet = -99 # Arbitrary low number that will always have matchups with higher net
+highestNetName = "PLACEHOLDER"
 
 for allyPkmn in allyPkmnLst:
     print (allyPkmn.toString())
-    # Calculate number of weaknesses
+    # Calculate number of weaknesses/strengths
+    # Weaknesses
     numWeaknesses = 0
     for foePkmn in foePkmnLst:
         if allyPkmn.isWeak(foePkmn):
             s = allyPkmn.name + " has weakness to " + foePkmn.name
             print (s)
             numWeaknesses += 1
-
-    # FIXME - randomize so equal number of weaknesses doesn't always select same pokemon
-    # Check if least weakn
-    if leastWeaknesses > numWeaknesses:
-        leastWeaknesses = numWeaknesses
-        lstWeakPkmnName = allyPkmn.name
-
-    # Print Pokemon weaknesses
-    s = allyPkmn.name + " has " + str(numWeaknesses) + " weaknesses" + "\n"
+    s = allyPkmn.name + " has " + str(numWeaknesses) + " weaknesses\n"
     print (s)
 
+    # Strengths
+    numStrengths = 0
+    for foePkmn in foePkmnLst:
+        if allyPkmn.isStrong(foePkmn):
+            s = allyPkmn.name + " has strength to " + foePkmn.name
+            print (s)
+            numStrengths += 1
+    s = allyPkmn.name + " has " + str(numStrengths) + " strengths\n\n"
+    print (s)
 
-# Print least weak pokemon
-print ("Select: ")
-print (lstWeakPkmnName)
-print ("with num weaknesses: ")
-print (leastWeaknesses)
+    # Calculate difference (net) in strengths vs weaknesses
+    net = numStrengths - numWeaknesses
+    if net > highestNet:
+        highestNet = net
+        highestNetName = allyPkmn.name
+
+
+# Print pokemon with best net
+s = highestNetName + " has net of " + str(highestNet)
+print (s)
