93f0a988bb532b62f0d037c16f188c88d6feaa6c
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-08-25 01:25:24 -0400

    Remove duplicate sets

--- RandomSetGenerationAnalysis/WriteGeneratedPokemon.py
+++ RandomSetGenerationAnalysis/WriteGeneratedPokemon.py
@@ -2,14 +2,17 @@
 
 # FIXME - clear out GeneratedGen8Pokemon directory
 
-# FIXME - don't write duplicate entries
+# FIXME - Verify that no sets accidently got deleted and that there are no duplicates
+
+
+import json
+import os
 
 
 OUT_FILE_PATH = "GeneratedGen8Pokemon/"
 DATA_FILE = "FormattedData.json"
+TEMP_FILE = OUT_FILE_PATH + "temp"
 
-
-import json
 
 f = open(DATA_FILE,)
 data = json.load(f)
@@ -21,5 +24,18 @@ f.close()
 for pkmn in data:
     filename = OUT_FILE_PATH + pkmn['name']
     with open(filename, 'a') as wfp:
+        pkmn['moves'].sort()
         wfp.write(json.dumps(pkmn))
         wfp.write("\n")
+
+    # Remove duplicate lines
+    # TODO - it would be better to just not write pokemon that are duplicates rather than remove them from the file after writing them
+    lines_seen = set() # holds lines already seen
+    outfile = open(TEMP_FILE, "w")
+    for line in open(filename, "r"):
+        if line not in lines_seen: # not a duplicate
+            outfile.write(line)
+            lines_seen.add(line)
+    outfile.close()
+    os.remove(filename)
+    os.rename(TEMP_FILE, filename)
