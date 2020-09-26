0e034ad6b22c8920616d6742bc441113e0cf002f
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-09-25 22:54:17 -0400

    Mostly hardcoded test case for generating a team that will have a counter for the 6 highest ranked pokemon

--- ConstraintSatisfactionTeamGeneration/Constraints.py
+++ ConstraintSatisfactionTeamGeneration/Constraints.py
@@ -1,4 +1,6 @@
 # Using viability rankings as data - https://www.smogon.com/forums/threads/ss-ou-viability-ranking-thread.3666340/
+# This is a part hardcoded part automated way of generating a team.
+# The algorithm used goes down the first 6 viability rankings and adds a pokemon to counter each.
 
 import sys
 sys.path.insert(1, '../SelectLead')
@@ -145,7 +147,7 @@ for line in data:
         # Touch up line
         line = line.replace("-", "")
         line = line.replace("UrshifuR", "Urshifu")
-        print (line)
+        #print (line)
         # Update data list
         types = pokedex.findPokemonUsingName(line.strip()).types
         pkmnLst.append ({
@@ -154,6 +156,78 @@ for line in data:
             "Rank": rank
         })
 
-# Print
+# Count amount of each type
+count = {
+    "Normal": 0,
+    "Fairy": 0,
+    "Poison": 0,
+    "Water": 0,
+    "Flying": 0,
+    "Steel": 0,
+    "Dragon": 0,
+    "Ghost": 0,
+    "Ground": 0,
+    "Fighting": 0,
+    "Dark": 0,
+    "Bug": 0,
+    "Fire": 0,
+    "Electric": 0,
+    "Grass": 0,
+    "Ice": 0,
+    "Psychic": 0,
+    "Rock": 0
+}
+
 for pkmn in pkmnLst:
+    #print (pkmn)
+    for type in pkmn['Types']:
+        count[type] += 1
+
+'''
+for type in types:
+    s = '{"type": "' + type + '", "count": 0},'
+    print (s)
+'''
+
+print (count)
+
+
+
+# Create team
+allyTeam = []
+
+# Find pokemon that is strong against clefable
+# Clefable (steel)
+for pkmn in pkmnLst:
+    if "Steel" in pkmn['Types'] and pkmn not in allyTeam:
+        allyTeam.append (pkmn)
+        break
+# Toxapex (electric)
+for pkmn in pkmnLst:
+    if "Electric" in pkmn['Types'] and pkmn not in allyTeam:
+        allyTeam.append (pkmn)
+        break
+# Corviknight (fire)
+for pkmn in pkmnLst:
+    if "Fire" in pkmn['Types'] and pkmn not in allyTeam:
+        allyTeam.append (pkmn)
+        break
+# Dragapult (fairy)
+for pkmn in pkmnLst:
+    if "Fairy" in pkmn['Types'] and pkmn not in allyTeam:
+        allyTeam.append (pkmn)
+        break
+# Excadrill (water)
+for pkmn in pkmnLst:
+    if "Water" in pkmn['Types'] and pkmn not in allyTeam:
+        allyTeam.append (pkmn)
+        break
+# Urshifu (fairy)
+# Skipped since clefable is already added and I thought that was good enough
+# Volcarona (rock)
+for pkmn in pkmnLst:
+    if "Rock" in pkmn['Types'] and pkmn not in allyTeam:
+        allyTeam.append (pkmn)
+        break
+for pkmn in allyTeam:
     print (pkmn)
