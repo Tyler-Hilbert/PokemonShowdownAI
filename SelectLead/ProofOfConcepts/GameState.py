7ebb4f390a67689a9b86c2735b533f6f8fef74c9
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-11-01 15:07:52 -0500

    Created game state

---
+++ SelectLead/ProofOfConcepts/GameState.py
@@ -0,0 +1,28 @@
+# Create tree of game states
+
+import sys
+sys.path.insert(1, '../')
+import Pokedex
+import Pokemon
+
+def search(allyPkmn, foePkmn, allyPkmnLst, foePkmnLst):
+    print ("\n\nallyPkmn", allyPkmn.toString(), "\nfoePkmn", foePkmn.toString(), "\nallyPkmnLst", allyPkmnLst, "\nfoePkmnLst", foePkmnLst)
+    score = allyPkmn.battle(foePkmn)
+    if score > 0:
+        foePkmnLst.remove(foePkmn)
+        for pkmn in foePkmnLst:
+            search(allyPkmn, pkmn, allyPkmnLst.copy(), foePkmnLst.copy())
+    elif score < 0:
+        allyPkmnLst.remove(allyPkmn)
+        for pkmn in allyPkmnLst:
+            search(pkmn, foePkmn, allyPkmnLst.copy(), foePkmnLst.copy())
+    else:
+        print ("Unknown winner")
+
+# Load Pokemon teams
+pokedex = Pokedex.Pokedex()
+allyPkmnLst = Pokemon.getPokemonInStr("Jolteon Espeon Flareon", pokedex)
+foePkmnLst = Pokemon.getPokemonInStr("Vaporeon Umbreon Leafeon", pokedex)
+# TODO -- loop through other ally pokemon
+for pkmn in foePkmnLst:
+    search (allyPkmnLst[0], pkmn, allyPkmnLst, foePkmnLst)
