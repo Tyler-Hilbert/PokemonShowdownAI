8ce0a8abe6ec501e132027d0e7a289c2c319fdf7
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-11-01 21:10:46 -0500

    Print tree structure in text in both debug and not debug

--- SelectLead/ProofOfConcepts/GameState.py
+++ SelectLead/ProofOfConcepts/GameState.py
@@ -5,19 +5,28 @@ sys.path.insert(1, '../')
 import Pokedex
 import Pokemon
 
-def search(allyPkmn, foePkmn, allyPkmnLst, foePkmnLst):
-    print ("\n\nallyPkmn:", allyPkmn.toString(), "\nfoePkmn:", foePkmn.toString(), "\nallyPkmnLst:\n", Pokemon.teamToString(allyPkmnLst), "foePkmnLst:\n", Pokemon.teamToString(foePkmnLst))
+DEBUG = False
+
+def search(allyPkmn, foePkmn, allyPkmnLst, foePkmnLst, depth):
+
+    if DEBUG:
+        print ("\n\n", "*-*"*depth)
+        print ("allyPkmn:", allyPkmn.toString(), "\nfoePkmn:", foePkmn.toString(), "\nallyPkmnLst:\n", Pokemon.teamToString(allyPkmnLst), "foePkmnLst:\n", Pokemon.teamToString(foePkmnLst))
+    else:
+        print ("-"*depth, "allyPkmn:", allyPkmn.toString().split('\t')[0], "foePkmn:", foePkmn.toString().split('\t')[0])
+
     score = allyPkmn.battle(foePkmn)
     if score > 0:
         foePkmnLst.remove(foePkmn)
         for pkmn in foePkmnLst:
-            search(allyPkmn, pkmn, allyPkmnLst.copy(), foePkmnLst.copy())
+            search(allyPkmn, pkmn, allyPkmnLst.copy(), foePkmnLst.copy(), depth+1)
     elif score < 0:
         allyPkmnLst.remove(allyPkmn)
         for pkmn in allyPkmnLst:
-            search(pkmn, foePkmn, allyPkmnLst.copy(), foePkmnLst.copy())
+            search(pkmn, foePkmn, allyPkmnLst.copy(), foePkmnLst.copy(), depth+1)
     else:
-        print ("Unknown winner")
+        if DEBUG:
+            print ("Unknown winner")
 
 # Load Pokemon teams
 pokedex = Pokedex.Pokedex()
@@ -25,4 +34,4 @@ allyPkmnLst = Pokemon.getPokemonInStr("Jolteon Espeon Flareon", pokedex)
 foePkmnLst = Pokemon.getPokemonInStr("Vaporeon Umbreon Leafeon", pokedex)
 # TODO -- loop through other ally pokemon
 for pkmn in foePkmnLst:
-    search (allyPkmnLst[0], pkmn, allyPkmnLst, foePkmnLst)
+    search (allyPkmnLst[0], pkmn, allyPkmnLst, foePkmnLst, 1)
